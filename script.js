d6a67c2aae43a78800524ebc9443c6de3d9731e6
7aGiven <fredrickareichert@gmail.com>
2024-10-05 18:45:16 +0800

    tab

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 function unityPlugin() {
 	const libUnityPlugin = Process.getModuleByName("libUnityPlugin.so")
-    Interceptor.attach(libUnityPlugin.getExportByName("_Z26il2cpp_get_global_metadataPKc"),{
-        onEnter(args) {
-        	const path = args[0].readCString()
-        	const reg = new RegExp("/data/(.*)/files")
-        	this.packageName = path.match(reg)[1]
-            console.log("il2cpp_get_global_metadata", path, args[1])
-        },
-        onLeave(retval) {
-            const p = retval.add(retval.add(8).readU32() - 8)
-            const size = p.readU32() + p.add(4).readU32()
-            const meta = Memory.dup(retval, size)
-            const stringOffset = meta.add(4*6).readU32()
-            const stringSize = meta.add(4*7).readU32()
-            const string = meta.add(stringOffset)
-            console.log("fix start")
-            let offset = 0
-            while (offset < stringSize) {
-                offset = buildString(string, offset)
-            }
-            console.log("fix end")
-            console.log("dump start")
-            const bytes = meta.readByteArray(size)
-            const path = `/data/data/${this.packageName}/global-metadata.dat`
-            File.writeAllBytes(path, bytes)
-            console.log("dump success", path)
-        }
-    })
+	Interceptor.attach(libUnityPlugin.getExportByName("_Z26il2cpp_get_global_metadataPKc"),{
+		onEnter(args) {
+			const path = args[0].readCString()
+			const reg = new RegExp("/data/(.*)/files")
+			this.packageName = path.match(reg)[1]
+			console.log("il2cpp_get_global_metadata", path, args[1])
+		},
+		onLeave(retval) {
+			const p = retval.add(retval.add(8).readU32() - 8)
+			const size = p.readU32() + p.add(4).readU32()
+			const meta = Memory.dup(retval, size)
+			const stringOffset = meta.add(4*6).readU32()
+			const stringSize = meta.add(4*7).readU32()
+			const string = meta.add(stringOffset)
+			console.log("fix start")
+			let offset = 0
+			while (offset < stringSize) {
+				offset = buildString(string, offset)
+			}
+			console.log("fix end")
+			console.log("dump start")
+			const bytes = meta.readByteArray(size)
+			const path = `/data/data/${this.packageName}/global-metadata.dat`
+			File.writeAllBytes(path, bytes)
+			console.log("dump success", path)
+		}
+	})
 }
 
 function buildString(string, offset) {
-    let xor = offset % 0xFF
-    do {
-        xor ^= string.add(offset).readU8()
-        string.add(offset).writeU8(xor)
-        offset++
-    } while (xor)
-    return offset
+	let xor = offset % 0xFF
+	do {
+		xor ^= string.add(offset).readU8()
+		string.add(offset).writeU8(xor)
+		offset++
+	} while (xor)
+	return offset
 }
 
 Interceptor.attach(Module.getExportByName("libdl.so","dlopen"),{
